69961cddfdf1359493411d0333489b2c5fbe5ebe
OE.omergunr100 <omer.er10@gmail.com>
2023-12-15 17:51:59 +0200

    Add project files.

---
+++ ThumperChaseMusic/Patches/CrawlerAIPatch.cs
@@ -0,0 +1,57 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ThumperChaseMusic.Patches
+{
+    [HarmonyPatch(typeof(CrawlerAI))]
+    internal class CrawlerAIPatch
+    {
+        private static AudioClip thumperSound = null;
+
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        public static void patchThumperChaseSoundStart(CrawlerAI __instance)
+        {
+            Plugin.log.LogInfo("Patching CrawlerAI...");
+            if (thumperSound == null)
+            {
+                AssetBundle ab = AssetBundle.LoadFromFile(Paths.PluginPath + "\\OE_Tweaks\\Sounds\\thumpersounds");
+                if (ab == null)
+                {
+                    Plugin.log.LogError("Failed to load thumpersounds asset bundle");
+                    return;
+                }
+                thumperSound = ab.LoadAsset<AudioClip>("thumper_chase.mp3");
+            }
+            __instance.GetComponent<AudioSource>().clip = thumperSound;
+            __instance.GetComponent<AudioSource>().volume = 0.4f;
+        }
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        public static void patchTumperChaseSoundUpdate(CrawlerAI __instance)
+        {
+            if (__instance.isEnemyDead)
+            {
+                __instance.GetComponent<AudioSource>().Stop();
+            }
+            if (__instance.currentBehaviourStateIndex == 0 && __instance.GetComponent<AudioSource>().isPlaying)
+            {
+                __instance.GetComponent<AudioSource>().Pause();
+            }
+            else if (__instance.currentBehaviourStateIndex != 0 && !__instance.GetComponent<AudioSource>().isPlaying)
+            {
+                if (__instance.GetComponent<AudioSource>().time > 0.0f && __instance.GetComponent<AudioSource>().time <= thumperSound.length - 0.1)
+                    __instance.GetComponent<AudioSource>().UnPause();
+                else
+                    __instance.GetComponent<AudioSource>().Play();
+            }
+        }
+    }
+}
---
+++ ThumperChaseMusic/Plugin.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThumperChaseMusic
+{
+    [BepInPlugin(GUID, NAME, VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony(GUID);
+
+        private const string GUID = "oe.tweaks.sounds.thumper";
+        private const string NAME = "Thumper Chase Music";
+        private const string VERSION = "1.0.0";
+
+        internal static Plugin instance;
+
+        internal static BepInEx.Logging.ManualLogSource log;
+
+        private void Awake()
+        {
+            log = this.Logger;
+            log.LogInfo("'Thumper Chase Music' is loading...");
+
+            if (instance == null)
+                instance = this;
+
+            harmony.PatchAll();
+
+            log.LogInfo("'Thumper Chase Music' loaded!");
+        }
+    }
+}
